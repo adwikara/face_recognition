53df6968337711a920a96ca8a672f60edc4c87d5
Aditya Wikara <adityawikara@gmail.com>
2019-07-17 19:15:00 +0700

    new face reg

--- detect.py
+++ detect.py
@@ -1,32 +1,23 @@
 import face_recognition
 import cv2
 import numpy as np
+import pandas as pd
+from sklearn.ensemble import IsolationForest
+import pickle
 
-# This is a demo of running face recognition on live video from your webcam. It's a little more complicated than the
-# other example, but it includes some basic performance tweaks to make things run a lot faster:
-#   1. Process each video frame at 1/4 resolution (though still display it at full resolution)
-#   2. Only detect faces in every other frame of video.
-
-# PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
-# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
-# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
+#Outlier Detection System
+with open("outlier_model", 'rb') as pickle_file:
+    clf = pickle.load(pickle_file)
 
 # Get a reference to webcam #0 (the default one)
 video_capture = cv2.VideoCapture(0)
 
+#Face Recognition Images
 # Load a sample picture and learn how to recognize it.
 Aditya_image = face_recognition.load_image_file("images/Aditya.jpg")
 Aditya_face_encoding = face_recognition.face_encodings(Aditya_image)[0]
 
 # Load a second sample picture and learn how to recognize it.
-Dadang_image = face_recognition.load_image_file("images/Dadang.jpg")
-Dadang_face_encoding = face_recognition.face_encodings(Dadang_image)[0]
-
-# Load a third sample picture and learn how to recognize it.
-Riri_image = face_recognition.load_image_file("images/Riri.jpg")
-Riri_face_encoding = face_recognition.face_encodings(Riri_image)[0]
-
-# Load a third sample picture and learn how to recognize it.
 Carrisa_image = face_recognition.load_image_file("images/Carrisa.jpg")
 Carrisa_face_encoding = face_recognition.face_encodings(Carrisa_image)[0]
 
@@ -34,26 +25,35 @@ Carrisa_face_encoding = face_recognition.face_encodings(Carrisa_image)[0]
 Esti_image = face_recognition.load_image_file("images/Esti.jpg")
 Esti_face_encoding = face_recognition.face_encodings(Esti_image)[0]
 
-# Load a third sample picture and learn how to recognize it.
+# Load a fourth sample picture and learn how to recognize it.
 Will_image = face_recognition.load_image_file("images/Will.jpg")
 Will_face_encoding = face_recognition.face_encodings(Will_image)[0]
+
+# Load a fourth sample picture and learn how to recognize it.
+Madeline_image = face_recognition.load_image_file("images/Madeline.jpg")
+Madeline_face_encoding = face_recognition.face_encodings(Madeline_image)[0]
+
+# Load a fourth sample picture and learn how to recognize it.
+Joshua_image = face_recognition.load_image_file("images/Joshua.jpg")
+Joshua_face_encoding = face_recognition.face_encodings(Joshua_image)[0]
+
 
 # Create arrays of known face encodings and their names
 known_face_encodings = [
     Aditya_face_encoding,
-    Dadang_face_encoding,
-    Riri_face_encoding,
     Carrisa_face_encoding,
     Esti_face_encoding,
-    Will_face_encoding
+    Will_face_encoding,
+    Joshua_face_encoding,
+    Madeline_face_encoding
 ]
 known_face_names = [
     "Aditya",
-    "Dadang",
-    "Riri",
     "Carrisa",
     "Esti",
-    "Will"
+    "Will",
+    "Joshua",
+    "Madeline"
 ]
 
 # Initialize some variables
@@ -92,7 +92,9 @@ while True:
             # Or instead, use the known face with the smallest distance to the new face
             face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
             best_match_index = np.argmin(face_distances)
-            if matches[best_match_index]:
+            if (clf.predict([face_encoding]) == -1):
+                name = "Unknown"
+            elif matches[best_match_index]:
                 name = known_face_names[best_match_index]
 
             face_names.append(name)
